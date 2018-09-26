dc71192ed6cb5c677c50c22a5af36822ebadd370
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-09-26 15:16:43 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180925140519) do
-
-  create_table "tweets", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20180925140502) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
