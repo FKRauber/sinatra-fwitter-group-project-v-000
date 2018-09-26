9e156ac98153edc45ac2d29f05e14b00559d0541
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-09-26 16:01:12 +0000

    automatically backed up by learn

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'pry'
 require 'spec_helper'
 describe 'User' do
   before do
-    @user = User.create(:username => "test 123", :email => "test123@aol.com", :password => "test")
+    @user = User.create(:username => "test 123", :email => "test123@aol.com", :password_digest => "test")
   end
   it 'can slug the username' do
     expect(@user.slug).to eq("test-123")
